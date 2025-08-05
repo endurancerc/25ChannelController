4f089739729d91fa04cada30e49bdb5921fed6ca
Endurance R/C <ppickett@gmail.com>
2025-08-05 16:12:52 -0500

    Add files via upload

---
+++ controller.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <windows.h>
+#include <stdio.h>
+#include <conio.h>
+#include <iostream>
+
+extern "C" {
+	#include "hidsdi.h"
+	#include <setupapi.h>
+}
+
+class controller {
+public:
+	controller(void);
+	~controller(void);
+
+	bool connected;
+bool send(int delay1,int delay2,int delay3,int delay4,int delay5,int delay6,int delay7,int delay8,
+					  int delay9,int delay10,int delay11,int delay12,int delay13,int delay14,int delay15,int delay16,
+					  int delay17,int delay18,int delay19,int delay20,int delay21,int delay22,int delay23,int delay24,
+					  int delay25);
+	bool connect();
+
+private:	
+
+	HANDLE								DeviceHandle;
+	HIDP_CAPS							Capabilities;
+	BOOL								result;
+
+	CHAR								OutputReport[26]; //buffer out to pctx or servo controller
+
+	DWORD								BytesWritten;
+	DWORD								BytesRead;
+};
